8d4c69a97b4115324515a96fdf775eccb746f24d
Matthew Flanagan <mattflanagan40@gmail.com>
2022-04-10 19:39:09 -0400

    bug fix

--- pages/videos.js
+++ pages/videos.js
@@ -7,7 +7,7 @@ export default function Videos(){
   <Layout>
     <Section className='videoContainer'>
     <iframe className='videoFrame' src="https://www.youtube.com/embed/0VEF-8gZBy4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    <iframe className='videoFrame' src="https://www.youtube.com/embed/5Gi1uZu9tWE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className='videoFrame' src="https://www.youtube.com/embed/5Gi1uZu9tWE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </Section>
   </Layout>
   );
