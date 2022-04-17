7fbc0d40233a7322ebd95d2641455cf02a731e00
Matthew Flanagan <mattflanagan40@gmail.com>
2022-04-17 15:51:29 -0400

    vitoimg bug fix

--- styles/globals.js
+++ styles/globals.js
@@ -262,7 +262,7 @@ Note: Beware of modifying this element as it can break the animations - you shou
   text-align: center;
 }
 
-.vitoImg img {
+.vitoImg {
   object-fit: contain;
   object-position: 50% 50%;
 }
